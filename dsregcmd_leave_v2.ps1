00883164ccf159e187e962cf0f2db17362a25685
Naures Shakarchi <naures@hotmail.com>
2022-09-08 15:28:13 +0200

    new scripts

---
+++ dsregcmd_leave_v2.ps1
@@ -0,0 +1,16 @@
+﻿###################################################################################################################
+# Created by: Naures Shakarchi                                                                                    #
+# Synopsis: Issues regarding Hybrid Domain Join pending state AAD object and re-creation of the AAD Device object #
+###################################################################################################################
+
+$Dsregcmd = New-Object PSObject ; Dsregcmd /status | Where {$_ -match ' : '}|ForEach {$Item = $_.Trim() -split '\s:\s'; $Dsregcmd|Add-Member -MemberType NoteProperty -Name $($Item[0] -replace '[:\s]','') -Value $Item[1] -EA SilentlyContinue}
+
+###########################################
+# Get status of Primary Refresh Token AAD #
+###########################################
+
+if ($dsregcmd.AzureAdJoined -eq "NO")
+{
+ dsregcmd /leave
+ schtasks /Run /TN “Microsoft\Windows\Workplace Join\Automatic-Device-Join”
+}
