a3364c732eb4d5fd6a64708a3e9413f263dc4417
Naures Shakarchi <naures@hotmail.com>
2022-09-15 12:49:28 +0200

    updatedscript

--- dsregcmd_leave_v2.ps1
+++ dsregcmd_leave_v2.ps1
@@ -1,16 +1,16 @@
-﻿###################################################################################################################
+###################################################################################################################
 # Created by: Naures Shakarchi                                                                                    #
 # Synopsis: Issues regarding Hybrid Domain Join pending state AAD object and re-creation of the AAD Device object #
 ###################################################################################################################
 
 $Dsregcmd = New-Object PSObject ; Dsregcmd /status | Where {$_ -match ' : '}|ForEach {$Item = $_.Trim() -split '\s:\s'; $Dsregcmd|Add-Member -MemberType NoteProperty -Name $($Item[0] -replace '[:\s]','') -Value $Item[1] -EA SilentlyContinue}
 
-###########################################
-# Get status of Primary Refresh Token AAD #
-###########################################
+##############################
+# Get the device AUTH status #
+##############################
 
-if ($dsregcmd.AzureAdJoined -eq "NO")
+if ($dsregcmd.DeviceAuthStatus -eq "FAILED")
 {
  dsregcmd /leave
- schtasks /Run /TN “Microsoft\Windows\Workplace Join\Automatic-Device-Join”
+ schtasks /Run /TN "Microsoft\Windows\Workplace Join\Automatic-Device-Join"
 }
